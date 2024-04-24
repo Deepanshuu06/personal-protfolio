84b01286620188c9abab0600b3da92f4d0b7e3c5
deepanshu baghel <106313726+Deepanshuu06@users.noreply.github.com>
2024-04-24 22:23:05 +0530

    creating the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { styles } from '../styles'
+import { Link, NavLink } from 'react-router-dom'
+import { logo, close, menu } from '../assets'
+import { navLinks } from '../constants/constants.js'
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
+  const [active, setActive] = useState('')
+  console.log(navLinks)
   return (
-    <div>Navbar</div>
+    <nav
+      className={`${styles.paddingX} w-full items-center py-5 fixed top-0 z20 bg-primary flex`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive('')
+            window.scrollTo(0, 0)
+          }}
+        >
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>
+            Deepanshu <span className='sm:block hidden'>| Protfolio</span>
+          </p>
+        </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map(link => (
+            <li
+              key={link.id}
+              className={`${active === link.title ? 'text-white' : 'text-secondary'} hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+        </div>
+      </div>
+    </nav>
   )
 }
 
